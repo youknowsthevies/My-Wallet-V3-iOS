07dd8d475513eda046e41fbae26c94b1f44e045f
Paulo <paulo@blockchain.com>
2020-02-19 08:04:54 +0000

    IOS-2747 - Adds doc

--- Blockchain/BackgroundTaskTimer.swift
+++ Blockchain/BackgroundTaskTimer.swift
@@ -9,36 +9,49 @@
 import Foundation
 import ToolKit
 
+/// BackgroundTaskTimer provides a way to delay the app suspension for a pre-defined amount of time
 class BackgroundTaskTimer {
 
-    var timer: Timer!
+    let timeInterval: TimeInterval
+    /// weak refernece to Timer added to the RunLoop
+    weak var timer: Timer!
     private var backgroundTaskID: UIBackgroundTaskIdentifier = .invalid
+
+    init(timeInterval: TimeInterval = 300) {
+        self.timeInterval = timeInterval
+    }
 
     var debugDescription: String {
         "\(self) timer: \(timer?.debugDescription ?? "nil"), backgroundTaskID: \(backgroundTaskID)"
     }
 
+    /// Stop timer
     func stop() {
         Logger.shared.debug("\(debugDescription)")
-        self.timer?.invalidate()
-        self.timer = nil
+        timer?.invalidate()
+        timer = nil
     }
 
+    /// Begin timer
     func begin(_ application: UIApplication, block: @escaping () -> Void) {
-        self.stop()
+        stop()
         Logger.shared.debug("\(debugDescription)")
-        self.backgroundTaskID = application.beginBackgroundTask(withName: "Delayed Background") {
-            self.timer?.invalidate()
-            self.timer = nil
+        /// Tells `application: UIApplication` that we are beginning a background task, expiration handler
+        /// stops timer, executes given block, and ends background task
+        backgroundTaskID = application.beginBackgroundTask(withName: "Delayed Background") {
+            self.stop()
             block()
             application.endBackgroundTask(self.backgroundTaskID)
             self.backgroundTaskID = .invalid
         }
-        self.timer = Timer(timeInterval: 4, repeats: false, block: { _ in
+        /// Creates timer that will execute block and end background task after a pre-defined amount of time
+        let timer = Timer(timeInterval: timeInterval, repeats: false, block: { _ in
             block()
             application.endBackgroundTask(self.backgroundTaskID)
             self.backgroundTaskID = .invalid
         })
+        self.timer = timer
+        /// Adds timer to current RunLoop
         RunLoop.current.add(timer, forMode: .default)
         Logger.shared.debug(" END \(debugDescription)")
     }
