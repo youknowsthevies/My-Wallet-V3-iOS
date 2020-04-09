70c1b42b83befc21371ace076862c3e1d2b0bc8b
Paulo <paulo@blockchain.com>
2020-04-09 18:03:18 +0100

    IOS-2463: Modules configs

--- Blockchain/Blockchain-Bridging-Header.h
+++ Blockchain/Blockchain-Bridging-Header.h
@@ -2,7 +2,6 @@
 //  Use this file to import your target's public headers that you would like to expose to Swift.
 //
 
-#import <UIKit/UIKit.h>
 #import <Localization/Localization-Swift.h>
 #import "BCWebViewController.h"
 #import "AccountsAndAddressesNavigationController.h"
