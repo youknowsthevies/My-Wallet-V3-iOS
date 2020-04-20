35176f58d7b1f6d346bb8787b064a48cb243690d
Paulo <paulo@blockchain.com>
2020-04-20 15:34:30 +0100

    IOS-3183: Bundling Veriff framework

---
+++ Veriff.framework/Headers/Veriff.h
@@ -0,0 +1,13 @@
+//
+//  Veriff.h
+//
+//  Copyright © 2019 Veriff. All rights reserved.
+//
+
+#import "Collector.h"
+
+//! Project version number for VeriffFramework.
+FOUNDATION_EXPORT double VeriffFrameworkVersionNumber;
+
+//! Project version string for VeriffFramework.
+FOUNDATION_EXPORT const unsigned char VeriffFrameworkVersionString[];
