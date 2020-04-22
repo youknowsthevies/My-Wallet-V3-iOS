92f57d48edcf8ee3d121da44da7d0d6c16d2445d
Paulo <paulo@blockchain.com>
2020-04-22 10:37:24 +0100

    IOS-3186: Add github issue URL to script

--- scripts/carthage-bootstrap.sh
+++ scripts/carthage-bootstrap.sh
@@ -6,7 +6,7 @@
 #  ------------
 #  Custom script to bootstrap carthage dependencies, that avoids erroring due to segmentation fault caused by PromiseKit.
 #  May want to reconsider the need of this on next Carthage release (current carthage release is 0.34.0)
-#
+#  https://github.com/Carthage/Carthage/issues/2760
 
 echo "try carthage bootstrap"
 carthage bootstrap --use-ssh --cache-builds --platform iOS || (echo "retry in 5" && sleep 5 && echo "retrying carthage bootstrap" && carthage bootstrap --use-ssh --cache-builds --platform iOS)
