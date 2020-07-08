07ecde37b840ea8e41dd772c4c70319857ae3384
paulo-bc <60097845+paulo-bc@users.noreply.github.com>
2020-07-08 15:45:49 +0100

    fix(NetworkCommunicator crash) IOS-3558: Removing AirDrop Services. (#1390)
    * IOS-3558: Removing AirDrop Services.
    
    * IOS-3558: Remove mock

--- Blockchain/DeepLinking/DeepLinkRouter.swift
+++ Blockchain/DeepLinking/DeepLinkRouter.swift
@@ -13,7 +13,6 @@ class DeepLinkRouter {
     private let routers: [DeepLinkRouting]
 
     init(routers: [DeepLinkRouting] = [
-        StellarAirdropRouter(),
         KYCDeepLinkRouter(),
         KYCResubmitIdentityRouter(),
         ExchangeDeepLinkRouter(),
