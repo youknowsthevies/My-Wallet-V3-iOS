67f08020eb5489ddf07b05eb36f664a0c297d900
Alex McGregor <41585563+thisisalexmcgregor@users.noreply.github.com>
2020-08-05 13:42:42 -0500

    chore(Interest): Adding InterestKit and InterestUIKit (#1472)
    * chore(Interest): Adding InterestKit and InterestUIKit
    
    * PR feedback

---
+++ InterestKit/InterestKit.h
@@ -0,0 +1,18 @@
+//
+//  InterestKit.h
+//  Blockchain
+//
+//  Created by Alex McGregor on 8/5/20.
+//  Copyright © 2020 Blockchain Luxembourg S.A. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for InterestKit.
+FOUNDATION_EXPORT double InterestKitVersionNumber;
+
+//! Project version string for InterestKit.
+FOUNDATION_EXPORT const unsigned char InterestKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <InterestKit/PublicHeader.h>
+
---
+++ InterestUIKit/InterestUIKit.h
@@ -0,0 +1,18 @@
+//
+//  InterestUIKit.h
+//  InterestUIKit
+//
+//  Created by Alex McGregor on 03/06/2020.
+//  Copyright © 2020 Blockchain Luxembourg S.A. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for InterestUIKit.
+FOUNDATION_EXPORT double InterestUIKitVersionNumber;
+
+//! Project version string for InterestUIKit.
+FOUNDATION_EXPORT const unsigned char InterestUIKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <InterestUIKit/PublicHeader.h>
+
