4241277c34d8891bacb7d13d96549bad2a57b3b2
paulo-bc <60097845+paulo-bc@users.noreply.github.com>
2020-10-14 09:55:07 +0100

    fix(Carthage) Update the Carthage vs Xcode 12 workaround. (#1623)
    * Update the Carthage vs Xcode 12 workaround.
    
    * Update the Carthage vs Xcode 12 workaround.

--- scripts/carthage-bootstrap.sh
+++ scripts/carthage-bootstrap.sh
@@ -4,25 +4,8 @@
 #
 #  What It Does
 #  ------------
-#  Workaround: Carthage builds fails at xcrun lipo on Xcode 12 beta (3,4,5...)
+#  Workaround: Carthage builds fails at xcrun lipo on Xcode 12
 #  https://github.com/Carthage/Carthage/issues/3019
+#  https://github.com/Carthage/Carthage/blob/master/Documentation/Xcode12Workaround.md
 
-xcconfig=$(mktemp /tmp/static.xcconfig.XXXXXX)
-trap 'rm -f "$xcconfig"' INT TERM HUP EXIT
-
-# For Xcode 12 make sure EXCLUDED_ARCHS is set to arm architectures otherwise
-# the build will fail on lipo due to duplicate architectures.
-# Xcode 12 GM
-echo 'EXCLUDED_ARCHS__EFFECTIVE_PLATFORM_SUFFIX_simulator__NATIVE_ARCH_64_BIT_x86_64__XCODE_1200__BUILD_12A7208 = arm64 arm64e armv7 armv7s armv6 armv8' >> $xcconfig
-# Xcode 12 GM 2
-echo 'EXCLUDED_ARCHS__EFFECTIVE_PLATFORM_SUFFIX_simulator__NATIVE_ARCH_64_BIT_x86_64__XCODE_1200__BUILD_12A7209 = arm64 arm64e armv7 armv7s armv6 armv8' >> $xcconfig
-echo 'EXCLUDED_ARCHS__EFFECTIVE_PLATFORM_SUFFIX_simulator__NATIVE_ARCH_64_BIT_x86_64__XCODE_1200 = $(EXCLUDED_ARCHS__EFFECTIVE_PLATFORM_SUFFIX_simulator__NATIVE_ARCH_64_BIT_x86_64__XCODE_1200__BUILD_$(XCODE_PRODUCT_BUILD_VERSION))' >> $xcconfig
-echo 'EXCLUDED_ARCHS = $(inherited) $(EXCLUDED_ARCHS__EFFECTIVE_PLATFORM_SUFFIX_$(EFFECTIVE_PLATFORM_SUFFIX)__NATIVE_ARCH_64_BIT_$(NATIVE_ARCH_64_BIT)__XCODE_$(XCODE_VERSION_MAJOR))' >> $xcconfig
-echo 'ONLY_ACTIVE_ARCH=NO' >> $xcconfig
-echo 'VALID_ARCHS = $(inherited) x86_64' >> $xcconfig
-export XCODE_XCCONFIG_FILE="$xcconfig"
-echo $XCODE_XCCONFIG_FILE
-
-carthage bootstrap --use-ssh --cache-builds --platform iOS || (echo "retry in 5" && sleep 5 && echo "retrying carthage bootstrap" && carthage bootstrap --use-ssh --cache-builds --platform iOS)
-
-exit 0
+sh scripts/carthage.sh bootstrap --use-ssh --cache-builds --platform iOS
