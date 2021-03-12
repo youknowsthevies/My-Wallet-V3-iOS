c3f468cf6e545d874503167b288c455995c1f7be
Alex McGregor <41585563+thisisalexmcgregor@users.noreply.github.com>
2021-03-12 14:53:14 -0600

    fix(IOS-4340): Additional PR comments (#1986)

--- PlatformKit/Services/Exchange/Domain/ExchangeAccountsClient.swift
+++ PlatformKit/Services/Exchange/Domain/ExchangeAccountsClient.swift
@@ -10,17 +10,11 @@ import DIKit
 import NetworkKit
 import RxSwift
 
-protocol ExchangeAccountStatusClientAPI {
-    var hasLinkedExchangeAccount: Single<Bool> { get }
-    var hasEnabled2FA: Single<Bool> { get }
-}
-
 protocol ExchangeAccountsProviderClientAPI {
     func exchangeAddress(with currency: CryptoCurrency) -> Single<CryptoExchangeAddressResponse>
 }
 
-protocol ExchangeAccountsClientAPI: ExchangeAccountStatusClientAPI,
-                                    ExchangeAccountsProviderClientAPI { }
+protocol ExchangeAccountsClientAPI: ExchangeAccountsProviderClientAPI { }
 
 final class ExchangeAccountsClient: ExchangeAccountsClientAPI {
     
@@ -35,21 +29,15 @@ final class ExchangeAccountsClient: ExchangeAccountsClientAPI {
     
     // MARK: - Properties
     
-    private let featureConfigurator: FeatureConfiguring
-    private let nabuUserService: NabuUserServiceAPI
     private let communicator: NetworkCommunicatorAPI
     private let requestBuilder: RequestBuilder
     
     // MARK: - Setup
     
-    init(featureConfigurator: FeatureConfiguring = resolve(),
-         nabuUserService: NabuUserServiceAPI = resolve(),
-         communicator: NetworkCommunicatorAPI = resolve(tag: DIKitContext.retail),
+    init(communicator: NetworkCommunicatorAPI = resolve(tag: DIKitContext.retail),
          requestBuilder: RequestBuilder = resolve(tag: DIKitContext.retail)) {
         self.communicator = communicator
-        self.featureConfigurator = featureConfigurator
         self.requestBuilder = requestBuilder
-        self.nabuUserService = nabuUserService
     }
     
     // MARK: - ExchangeAccountsClientAPI
@@ -62,34 +50,5 @@ final class ExchangeAccountsClient: ExchangeAccountsClientAPI {
             authenticated: true
         )!
         return communicator.perform(request: request)
-    }
-    
-    var hasLinkedExchangeAccount: Single<Bool> {
-        nabuUserService
-            .fetchUser()
-            .map(\.hasLinkedExchangeAccount)
-    }
-    
-    var hasEnabled2FA: Single<Bool> {
-        /// It does not matter what asset we fetch.
-        exchangeAddress(with: .bitcoin)
-            /// If the user has accounts returned,
-            /// then they have 2FA enabled.
-            .map { _ in true }
-            /// If an error is thrown when fetching accounts
-            /// parse the error to determine if it is because 2FA is
-            /// not enabled.
-            .catchError { error in
-                guard let networkError = error as? NetworkCommunicatorError else {
-                    throw error
-                }
-                if case let .serverError(serverError) = networkError,
-                   let nabuError = serverError.nabuError,
-                   nabuError.code == .bad2fa {
-                    return .just(false)
-                } else {
-                    throw networkError
-                }
-            }
     }
 }
--- PlatformKit/Services/Exchange/Network/ExchangeAccountStatusService.swift
+++ PlatformKit/Services/Exchange/Network/ExchangeAccountStatusService.swift
@@ -21,20 +21,44 @@ public final class ExchangeAccountStatusService: ExchangeAccountStatusServiceAPI
     // MARK: - ExchangeLinkStatusServiceAPI
     
     public var hasLinkedExchangeAccount: Single<Bool> {
-        client.hasLinkedExchangeAccount
+        nabuUserService
+            .fetchUser()
+            .map(\.hasLinkedExchangeAccount)
     }
     
     public var hasEnabled2FA: Single<Bool> {
-        client.hasEnabled2FA
+        /// It does not matter what asset we fetch.
+        client.exchangeAddress(with: .bitcoin)
+            /// If the user has accounts returned,
+            /// then they have 2FA enabled.
+            .map { _ in true }
+            /// If an error is thrown when fetching accounts
+            /// parse the error to determine if it is because 2FA is
+            /// not enabled.
+            .catchError { error in
+                guard let networkError = error as? NetworkCommunicatorError else {
+                    throw error
+                }
+                if case let .serverError(serverError) = networkError,
+                   let nabuError = serverError.nabuError,
+                   nabuError.code == .bad2fa {
+                    return .just(false)
+                } else {
+                    throw networkError
+                }
+            }
     }
     
     // MARK: - Private Properties
     
-    private let client: ExchangeAccountStatusClientAPI
+    private let nabuUserService: NabuUserServiceAPI
+    private let client: ExchangeAccountsProviderClientAPI
     
     // MARK: - Init
     
-    init(client: ExchangeAccountsClientAPI = resolve()) {
+    init(nabuUserService: NabuUserServiceAPI = resolve(),
+         client: ExchangeAccountsClientAPI = resolve()) {
+        self.nabuUserService = nabuUserService
         self.client = client
     }
 }
