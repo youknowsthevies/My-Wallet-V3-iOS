c978810e34f5765c898444162ab60e47c3a063d8
Gianluca Tranchedone <gtranchedone@blockchain.com>
2021-04-26 17:18:36 +0100

    Remove unneeded conformance of String to PickerViewSelectable

--- Modules/KYCFeature/KYCUIKit/Address/PickerViewSelectable.swift
+++ Modules/KYCFeature/KYCUIKit/Address/PickerViewSelectable.swift
@@ -2,14 +2,3 @@ protocol PickerViewSelectable {
     var id: String { get }
     var title: String { get }
 }
-
-extension String: PickerViewSelectable {
-    
-    var id: String {
-        self
-    }
-    
-    var title: String {
-        self
-    }
-}
