a8c885b08578331416162a091b091255355e7840
Alex McGregor <41585563+thisisalexmcgregor@users.noreply.github.com>
2021-04-26 11:04:00 -0500

    feat(ACH): FiatWithdrawalTransactionEngine & FiatDepositTransactionEngine (#2090)

---
+++ Modules/TransactionFeature/TransactionKit/TransactionAPI/TransactionProcessor/TransactionEngine/FiatWithdrawalTransactionEngine.swift
@@ -0,0 +1,148 @@
+//
+//  FiatWithdrawalTransactionEngine.swift
+//  TransactionKit
+//
+//  Created by Alex McGregor on 4/21/21.
+//  Copyright © 2021 Blockchain Luxembourg S.A. All rights reserved.
+//
+
+import DIKit
+import PlatformKit
+import RxSwift
+import ToolKit
+
+final class FiatWithdrawalTransactionEngine: TransactionEngine {
+    
+    var fiatExchangeRatePairs: Observable<TransactionMoneyValuePairs> {
+        .empty()
+    }
+    
+    let fiatCurrencyService: FiatCurrencyServiceAPI
+    let priceService: PriceServiceAPI
+    let requireSecondPassword: Bool = false
+    var askForRefreshConfirmation: ((Bool) -> Completable)!
+    var sourceAccount: BlockchainAccount!
+    var transactionTarget: TransactionTarget!
+
+    var sourceTradingAccount: FiatAccount! {
+        sourceAccount as? FiatAccount
+    }
+    
+    var target: LinkedBankAccount { transactionTarget as! LinkedBankAccount }
+    var targetAsset: FiatCurrency { target.fiatCurrency }
+    var sourceAsset: FiatCurrency { sourceTradingAccount.fiatCurrency }
+    
+    // MARK: - Init
+
+    init(fiatCurrencyService: FiatCurrencyServiceAPI = resolve(),
+         priceService: PriceServiceAPI = resolve()) {
+        self.fiatCurrencyService = fiatCurrencyService
+        self.priceService = priceService
+    }
+    
+    // MARK: - TransactionEngine
+    
+    func assertInputsValid() {
+        precondition(sourceAccount is FiatAccount)
+        precondition(transactionTarget is LinkedBankAccount)
+    }
+    
+    func initializeTransaction() -> Single<PendingTransaction> {
+        Single.zip(sourceAccount.actionableBalance,
+                   sourceAccount.balance,
+                   target.withdrawFeeAndMinLimit,
+                   fiatCurrencyService
+                       .fiatCurrency)
+            .map(weak: self) { (self, values) -> PendingTransaction in
+                let (actionableBalance, balance, feeAndLimit, fiatCurrency) = values
+                let zero: MoneyValue = .zero(currency: self.sourceAsset)
+                return PendingTransaction(
+                    amount: zero,
+                    // TODO: Total balance?
+                    available: actionableBalance,
+                    feeAmount: feeAndLimit.fee.moneyValue,
+                    feeForFullAvailable: zero,
+                    feeSelection: .init(
+                        selectedLevel: .none,
+                        availableLevels: []
+                    ),
+                    selectedFiatCurrency: fiatCurrency,
+                    minimumLimit: feeAndLimit.minLimit.moneyValue,
+                    maximumLimit: actionableBalance
+                )
+            }
+    }
+    
+    func doBuildConfirmations(pendingTransaction: PendingTransaction) -> Single<PendingTransaction> {
+        .just(pendingTransaction
+                .insert(
+                    confirmations: [
+                        .source(.init(value: sourceAccount.label)),
+                        .destination(.init(value: target.label)),
+                        // TODO: Fee
+                        // TODO: EstimatedWithdrawalCompletion
+                        .total(.init(total: pendingTransaction.amount))
+                    ]
+                )
+        )
+    }
+    
+    func update(amount: MoneyValue, pendingTransaction: PendingTransaction) -> Single<PendingTransaction> {
+        .just(pendingTransaction.update(amount: amount))
+    }
+    
+    func validateAmount(pendingTransaction: PendingTransaction) -> Single<PendingTransaction> {
+        if pendingTransaction.validationState == .uninitialized && pendingTransaction.amount.isZero {
+            return .just(pendingTransaction)
+        } else {
+            return validateAmountCompletable(pendingTransaction: pendingTransaction)
+                .updateTxValidityCompletable(pendingTransaction: pendingTransaction)
+        }
+    }
+    
+    func doValidateAll(pendingTransaction: PendingTransaction) -> Single<PendingTransaction> {
+        validateAmount(pendingTransaction: pendingTransaction)
+            .updateTxValiditySingle(pendingTransaction: pendingTransaction)
+    }
+    
+    func execute(pendingTransaction: PendingTransaction, secondPassword: String) -> Single<TransactionResult> {
+        target
+            .receiveAddress
+            .flatMapCompletable(weak: self) { (self, receiveAddress) -> Completable in
+                // TODO: Create withdraw order
+                unimplemented()
+            }
+            .flatMapSingle {
+                .just(TransactionResult.unHashed(amount: pendingTransaction.amount))
+            }
+    }
+    
+    func doPostExecute(transactionResult: TransactionResult) -> Completable {
+        unimplemented()
+    }
+    
+    func doUpdateFeeLevel(pendingTransaction: PendingTransaction, level: FeeLevel, customFeeAmount: MoneyValue) -> Single<PendingTransaction> {
+        precondition(pendingTransaction.feeSelection.availableLevels.contains(level))
+        return .just(pendingTransaction)
+    }
+    
+    // MARK: - Private Functions
+    
+    private func validateAmountCompletable(pendingTransaction: PendingTransaction) -> Completable {
+        Completable.fromCallable {
+            guard let maxLimit = pendingTransaction.maximumLimit,
+                  let minLimit = pendingTransaction.minimumLimit else {
+                throw TransactionValidationFailure(state: .unknownError)
+            }
+            guard try pendingTransaction.amount < minLimit else {
+                throw TransactionValidationFailure(state: .belowMinimumLimit)
+            }
+            guard try pendingTransaction.amount > maxLimit else {
+                throw TransactionValidationFailure(state: .overMaximumLimit)
+            }
+            guard try pendingTransaction.available < pendingTransaction.amount else {
+                throw TransactionValidationFailure(state: .insufficientFunds)
+            }
+        }
+    }
+}
