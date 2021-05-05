32c17dd220ef6279341ca2057a796b74ddfe8167
Maciej Burda <mburda@blockchain.com>
2021-05-05 15:37:29 +0200

    IOS-4508: Add new target to project

---
+++ Modules/Analytics/NabuAnalyticsKit/DIKit.swift
@@ -0,0 +1,10 @@
+// Copyright © Blockchain Luxembourg S.A. All rights reserved.
+
+import DIKit
+
+extension DependencyContainer {
+     
+    public static var nabuAnalyticsKit = module {
+
+    }
+}
