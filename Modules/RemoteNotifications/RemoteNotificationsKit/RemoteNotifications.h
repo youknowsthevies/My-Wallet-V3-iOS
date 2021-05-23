cb4df9d15df4cedc1586aec25fc1dcad5de1bdd8
grayson-bc <grayson@blockchain.com>
2021-05-23 16:46:18 +0800

    IOS-4700: Add RemoteNotificationsKit Framework

---
+++ Modules/RemoteNotifications/RemoteNotificationsKit/RemoteNotifications.h
@@ -0,0 +1,13 @@
+// Copyright © Blockchain Luxembourg S.A. All rights reserved.
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for RemoteNotifications.
+FOUNDATION_EXPORT double RemoteNotificationsVersionNumber;
+
+//! Project version string for RemoteNotifications.
+FOUNDATION_EXPORT const unsigned char RemoteNotificationsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RemoteNotifications/PublicHeader.h>
+
+
