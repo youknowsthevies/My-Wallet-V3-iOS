67b541cbd7cd38511a69433e7e8034656a5c3125
Gianluca Tranchedone <gtranchedone@blockchain.com>
2021-05-26 19:18:24 +0100

    fix(NetworkKit) Fix crash in 4.0.3 due to NetworkRequest's description

--- Modules/Network/NetworkKit/General/Request/NetworkRequest.swift
+++ Modules/Network/NetworkKit/General/Request/NetworkRequest.swift
@@ -128,7 +128,7 @@ public struct NetworkRequest {
 extension NetworkRequest: CustomStringConvertible {
 
     public var description: String {
-        "\(type(of: self)) underlying URLRequest: \(urlRequest)"
+        "\(method.rawValue) \(endpoint) (\(authenticated ? "authenticated" : "unauthenticated"))"
     }
 }
 
