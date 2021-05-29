041975c3a8f96ce08bfb2846fd96929c139df42b
grayson-bc <grayson@blockchain.com>
2021-05-30 01:00:15 +0800

    IOS-4730: Create SSO framework and Folder Structure

---
+++ Modules/SingleSignOnFeature/SingleSignOnKit/SingleSignOnKit.h
@@ -0,0 +1,13 @@
+// Copyright © Blockchain Luxembourg S.A. All rights reserved.
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SingleSignOnKit.
+FOUNDATION_EXPORT double SingleSignOnKitVersionNumber;
+
+//! Project version string for SingleSignOnKit.
+FOUNDATION_EXPORT const unsigned char SingleSignOnKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SingleSignOnKit/PublicHeader.h>
+
+
