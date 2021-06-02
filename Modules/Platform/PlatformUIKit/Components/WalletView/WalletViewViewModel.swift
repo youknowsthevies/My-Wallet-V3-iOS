c9752eb87967d3fed34118f0023e4cabcaf5ba03
Maciej Burda <mburda@blockchain.com>
2021-06-02 16:12:41 +0200

    IOS-3154: Fix double dot for some accessibility ids

--- Modules/Platform/PlatformUIKit/Components/WalletView/WalletViewViewModel.swift
+++ Modules/Platform/PlatformUIKit/Components/WalletView/WalletViewViewModel.swift
@@ -83,17 +83,17 @@ final class WalletViewViewModel {
         }
 
         balanceLabelContent = account
-                    .balance
-                    .map(\.displayString)
-                    .map { value in
-                        .init(
-                            text: value,
-                            font: .main(.medium, 14.0),
-                            color: .descriptionText,
-                            alignment: .left,
-                            accessibility: .id("\(descriptor.accessibilityPrefix).wallet.balance")
-                        )
-                    }
-                    .asDriver(onErrorJustReturn: .empty)
+            .balance
+            .map(\.displayString)
+            .map { value in
+                .init(
+                    text: value,
+                    font: .main(.medium, 14.0),
+                    color: .descriptionText,
+                    alignment: .left,
+                    accessibility: .id("\(descriptor.accessibilityPrefix).wallet.balance")
+                )
+            }
+            .asDriver(onErrorJustReturn: .empty)
     }
 }
