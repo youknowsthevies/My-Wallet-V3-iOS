a6f28d4591f713a2051b7538b90d97be621e1d10
grayson-bc <grayson@blockchain.com>
2021-06-02 15:26:55 +0800

    IOS-4482: Verify Device View Stub Code

---
+++ Modules/AuthenticationFeature/AuthenticationUIKit/Login/VerifyDeviceView.swift
@@ -0,0 +1,19 @@
+// Copyright © Blockchain Luxembourg S.A. All rights reserved.
+
+import AuthenticationKit
+import ComposableArchitecture
+import Localization
+import UIComponentsKit
+import SwiftUI
+
+public struct VerifyDeviceView: View {
+    public var body: some View {
+        VStack {}
+    }
+}
+
+struct VerifyDeviceView_Previews: PreviewProvider {
+    static var previews: some View {
+        VerifyDeviceView()
+    }
+}
