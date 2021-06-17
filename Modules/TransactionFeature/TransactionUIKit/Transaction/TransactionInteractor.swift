6920142d70ffe6a28ac0e29aa571984caf39331e
Cosmin Baies <82030038+cbaies-bc@users.noreply.github.com>
2021-06-17 23:25:02 +0300

    fix: Fix build issues (#2297)

--- Modules/TransactionFeature/TransactionUIKit/Transaction/TransactionInteractor.swift
+++ Modules/TransactionFeature/TransactionUIKit/Transaction/TransactionInteractor.swift
@@ -24,7 +24,7 @@ final class TransactionInteractor {
         }
     }
 
-    private let coincore: Coincore
+    private let coincore: CoincoreAPI
     private let availablePairsService: AvailableTradingPairsServiceAPI
     private let swapEligibilityService: EligibilityServiceAPI
     private let linkedBanksFactory: LinkedBanksFactoryAPI
@@ -35,7 +35,7 @@ final class TransactionInteractor {
     private let invalidate = PublishSubject<Void>()
 
     init(
-        coincore: Coincore = resolve(),
+        coincore: CoincoreAPI = resolve(),
         availablePairsService: AvailableTradingPairsServiceAPI = resolve(),
         swapEligibilityService: EligibilityServiceAPI = resolve(),
         linkedBanksFactory: LinkedBanksFactoryAPI = resolve(),
