fc49f14c916c3882ada8fca03ffeb3d55029c895
grayson-bc <grayson@blockchain.com>
2021-07-16 17:18:13 +0800

    Remove Launch Argument and Use new app delegate

--- Blockchain/main.swift
+++ Blockchain/main.swift
@@ -5,15 +5,7 @@ import UIKit
 /// temporary top-level decision based on environment variable `bootstrap_work`
 /// to either use the new `AppDelegate` which uses `ComposableArchitecture` or the older `BlockchainAppDelegate`
 var appDelegateClass: String {
-    guard let bootstrapWork = ProcessInfo.processInfo.environment["bootstrap_work"] else {
-        return NSStringFromClass(BlockchainAppDelegate.self)
-    }
-    guard let shouldUseNewBootstrap = Bool(bootstrapWork.lowercased()) else {
-        return NSStringFromClass(BlockchainAppDelegate.self)
-    }
-    return shouldUseNewBootstrap
-        ? NSStringFromClass(AppDelegate.self)
-        : NSStringFromClass(BlockchainAppDelegate.self)
+    NSStringFromClass(AppDelegate.self)
 }
 
 UIApplicationMain(
