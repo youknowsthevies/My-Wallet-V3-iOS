715c0c76840c1121fd5087731bb83a0c880ded9d
dchatzieleftheriou-bc <71259279+dchatzieleftheriou-bc@users.noreply.github.com>
2021-08-14 14:09:12 +0300

    IOS-5179: Fixes an issue with logged in second password wallets (#2553)

--- BlockchainTests/AppFeature/MainAppReducerTests.swift
+++ BlockchainTests/AppFeature/MainAppReducerTests.swift
@@ -277,6 +277,7 @@ final class MainAppReducerTests: XCTestCase {
         XCTAssertNil(mockSettingsApp.guid)
         XCTAssertNil(mockSettingsApp.sharedKey)
 
+        testStore.receive(.onboarding(.informSecondPasswordDetected))
         testStore.receive(.onboarding(.welcomeScreen(.informSecondPasswordDetected))) { state in
             state.onboarding?.welcomeState?.screenFlow = .welcomeScreen
             state.onboarding?.welcomeState?.modals = .secondPasswordNoticeScreen
