7a4c8c1bd7a5c77a0572c0615697cea268420f16
Oliver Atkinson <88668746+oliveratkinson-bc@users.noreply.github.com>
2021-08-23 12:54:54 +0100

    Add Brewfile to specify  an exact installed version across many systems (#2581)

--- scripts/install-brew-dependencies.sh
+++ scripts/install-brew-dependencies.sh
@@ -11,4 +11,4 @@ set -u
 
 git -C "/usr/local/Homebrew/Library/Taps/homebrew/homebrew-core" remote set-url origin https://github.com/Homebrew/homebrew-core
 brew update
-brew install autoconf automake carthage gnu-sed pkg-config swiftlint libtool xcodegen swiftformat swift-sh
+brew bundle install
