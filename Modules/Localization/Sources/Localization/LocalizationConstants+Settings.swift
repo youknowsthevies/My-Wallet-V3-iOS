f1f5d2ca3e8f163ff6fd51d8fed83f1f8850c44c
grayson-bc <83952192+grayson-bc@users.noreply.github.com>
2021-09-21 13:09:32 +0100

    IOS-5354: Fix capitalisation (#2718)

--- Modules/Localization/Sources/Localization/LocalizationConstants+Settings.swift
+++ Modules/Localization/Sources/Localization/LocalizationConstants+Settings.swift
@@ -30,7 +30,7 @@ extension LocalizationConstants.Settings {
         public static let mobileNumber = NSLocalizedString("Mobile Number", comment: "Mobile Number")
         public static let email = NSLocalizedString("Email", comment: "Email")
         public static let blockchainExchange = NSLocalizedString("Blockchain Exchange", comment: "Blockchain Exchange")
-        public static let recoveryPhrase = NSLocalizedString("Backup phrase", comment: "Backup phrase")
+        public static let recoveryPhrase = NSLocalizedString("Backup Phrase", comment: "Backup phrase")
         public static let confirmed = NSLocalizedString("Confirmed", comment: "Confirmed")
         public static let unconfirmed = NSLocalizedString("Unconfirmed", comment: "Unconfirmed")
         public static let localCurrency = NSLocalizedString("Local Currency", comment: "Local Currency")
