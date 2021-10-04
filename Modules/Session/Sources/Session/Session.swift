6b055ab9146e48479c344420b4f9980ff41f3537
Oliver Atkinson <88668746+oliveratkinson-bc@users.noreply.github.com>
2021-10-04 09:36:52 +0100

    Update Session.swift

--- Modules/Session/Sources/Session/Session.swift
+++ Modules/Session/Sources/Session/Session.swift
@@ -15,9 +15,15 @@ public final class State<Key: Hashable> {
 extension State {
 
     public class Data {
+
         public internal(set) var store: [Key: Any] = [:]
-        var subjects: [Key: Subject] = [:]
+        internal var subjects: [Key: Subject] = [:]
         private var dirty: (data: [Key: Any], level: UInt) = ([:], 0)
+
+        private var q: DispatchQueue = DispatchQueue(label: "com.blockchain.session.state.q")
+        private var k: DispatchSpecificKey<Data.Type>
+
+        init() { k = .init(on: q) }
     }
 
     public enum Error: Swift.Error {
@@ -73,7 +79,7 @@ extension State {
         return value
     }
 
-    private func get(_ key: Key) throws -> Any {
+    public func get(_ key: Key) throws -> Any {
         try data.get(key)
     }
 
@@ -92,25 +98,26 @@ extension State {
     }
 
     public func publisher<T: Equatable>(for key: Key, as _: T.Type = T.self) -> AnyPublisher<Result<T, Error>, Never> {
-        publisher(for: key).map { result in
-            Self.map(result, to: T.self, for: key)
-        }.removeDuplicates(by: { a, b in
-            switch (a, b) {
-            case let (.success(a), .success(b)):
-                return a == b
-            case let (.failure(.keyDoesNotExist(a)), .failure(.keyDoesNotExist(b))):
-                return a == b
-            default:
-                return false
+        publisher(for: key)
+            .map { result in
+                Self.map(result, to: T.self, for: key)
             }
-        }).eraseToAnyPublisher()
+            .removeDuplicates(by: { a, b in
+                switch (a, b) {
+                case let (.success(a), .success(b)):
+                    return a == b
+                case let (.failure(.keyDoesNotExist(a)), .failure(.keyDoesNotExist(b))):
+                    return a == b
+                default:
+                    return false
+                }
+            })
+            .eraseToAnyPublisher()
     }
 
     public func publisher(for key: Key) -> AnyPublisher<Result<Any, Error>, Never> {
-        let subject = data.subjects[key, default: Subject()]
-        defer { data.subjects[key] = subject }
         return Just(result(for: key))
-            .merge(with: subject)
+            .merge(with: data.subject(for: key))
             .eraseToAnyPublisher()
     }
 
@@ -126,75 +133,113 @@ extension State {
 
 extension State.Data {
 
-    public var isInTransaction: Bool { dirty.level > 0 }
+    public var isInTransaction: Bool { sync { dirty.level > 0 } }
     public var isNotInTransaction: Bool { !isInTransaction }
 
+    public func contains(_ key: Key) -> Bool {
+        sync { store.keys.contains(key) }
+    }
+
     func get(_ key: Key) throws -> Any {
-        guard let value = store[key] else {
+        guard let value = sync(execute: { store[key] }) else {
             throw State.Error.keyDoesNotExist(key)
         }
         return try (value as? Computed)?.yield() ?? value
     }
 
     func set(_ key: Key, to value: Any) {
-        dirty.data[key] = value
-        if isNotInTransaction {
-            store[key] = value
-            updateSubjects()
+        sync {
+            dirty.data[key] = value
+            if isNotInTransaction {
+                store[key] = value
+                updateSubjects()
+            }
         }
     }
 
     func clear(_ key: Key) {
-        if isInTransaction {
-            dirty.data[key] = Tombstone.self
-        } else {
-            store[key] = nil
-            updateSubjects()
+        sync {
+            if isInTransaction {
+                dirty.data[key] = Tombstone.self
+            } else {
+                store[key] = nil
+                updateSubjects()
+            }
         }
     }
 
     func beginTransaction() {
-        dirty.level += 1
+        sync { dirty.level += 1 }
     }
 
     func endTransaction() {
-        switch dirty.level {
-        case 1:
-            updateSubjects()
-        case 1...UInt.max:
-            dirty.level -= 1
-        default:
-            return assertionFailure("Misaligned begin -> end transaction calls. You must be in a transaction to end a transaction.")
+        sync {
+            switch dirty.level {
+            case 1:
+                updateSubjects()
+            case 1...UInt.max:
+                dirty.level -= 1
+            default:
+                assertionFailure(
+                    "Misaligned begin -> end transaction calls. You must be in a transaction to end a transaction."
+                )
+            }
+            dirty.data = [:]
         }
-        dirty.data = [:]
     }
 
     func rollbackTransaction() {
-        precondition(isInTransaction)
-        dirty.level = 0
-        dirty.data = [:]
+        sync {
+            precondition(isInTransaction)
+            dirty.level = 0
+            dirty.data = [:]
+        }
     }
 
-    func updateSubjects() {
-        for (key, value) in dirty.data {
-            switch value {
-            case is Tombstone.Type:
-                store.removeValue(forKey: key)
-                notify(key, with: Optional<Any>.none as Any)
-            default:
-                store[key] = value
-                notify(key, with: value)
-            }
+    func subject(for key: Key) -> State.Subject {
+        sync {
+            let subject = subjects[key, default: .init()]
+            subjects[key] = subject
+            return subject
         }
-        dirty.level = 0
-        dirty.data.removeAll(keepingCapacity: true)
+    }
+
+    private func updateSubjects() {
+        sync {
+            for (key, value) in dirty.data {
+                switch value {
+                case is Tombstone.Type:
+                    store.removeValue(forKey: key)
+                    notify(key, with: Any?.none as Any)
+                default:
+                    store[key] = value
+                    notify(key, with: value)
+                }
+            }
+            dirty.level = 0
+            dirty.data.removeAll(keepingCapacity: true)
+        }
     }
 
     func notify(_ key: Key, with value: Any) {
         subjects[key]?.send(.success(value))
     }
+
+    @discardableResult
+    public func sync<T>(execute work: () throws -> T) rethrows -> T {
+        DispatchQueue.getSpecific(key: k) == nil
+            ? try q.sync(execute: work)
+            : try work()
+    }
 }
 
 extension State {
     public typealias Subject = PassthroughSubject<Result<Any, Error>, Never>
+}
+
+extension DispatchSpecificKey {
+    convenience init<K>(_: K.Type = K.self, on queue: DispatchQueue) where T == K.Type {
+        self.init()
+        queue.setSpecific(key: self, value: K.self)
+    }
 }
