aefcec848120ce5485d12b5a138141d70fe9886b
Maciej Burda <81174411+mburda-bc@users.noreply.github.com>
2021-10-11 13:13:58 +0200

    Add user defaults persistence for static feature flag. (#2850)

--- Modules/Tool/Sources/ToolKit/FeatureFlags/StaticFeatureFlags.swift
+++ Modules/Tool/Sources/ToolKit/FeatureFlags/StaticFeatureFlags.swift
@@ -3,5 +3,17 @@
 import Foundation
 
 public enum StaticFeatureFlags {
-    public static var isDynamicAssetsEnabled = false
+
+    private enum UserDefaultsKeys {
+        static let dynamicAssetsEnabledKey = "StaticFeatureFlags.UserDefaultsKeys.dynamicAssetsEnabledKey"
+    }
+
+    public static var isDynamicAssetsEnabled: Bool {
+        get {
+            UserDefaults.standard.bool(forKey: UserDefaultsKeys.dynamicAssetsEnabledKey)
+        }
+        set {
+            UserDefaults.standard.set(newValue, forKey: UserDefaultsKeys.dynamicAssetsEnabledKey)
+        }
+    }
 }
