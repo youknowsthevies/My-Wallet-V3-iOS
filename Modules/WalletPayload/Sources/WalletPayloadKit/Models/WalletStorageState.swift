e64f2fd3d07db27f0166f5ca799e5a35fe6c6457
dchatzieleftheriou-bc <71259279+dchatzieleftheriou-bc@users.noreply.github.com>
2021-10-13 18:37:38 +0300

    feature(WalletPayload): IOS-5473 Wallet Storage implementation (#2863)
    * IOs-5473 Adds initial implementation of WalletFetcher
    
    * IOS-5473 Adds WalletError
    
    * IOS-5473 Adds WalletStorage and WalletStorageState
    
    * IOS-5473 run swiftformat
    
    * IOs-5473 removes redundant use of self

---
+++ Modules/WalletPayload/Sources/WalletPayloadKit/Models/WalletStorageState.swift
@@ -0,0 +1,49 @@
+// Copyright © Blockchain Luxembourg S.A. All rights reserved.
+
+import Foundation
+
+/// Holds the storage for wallet related access
+public struct WalletStorageState: Equatable {
+
+    /// Stores credential related properties
+    public var credentials: WalletCredentials
+
+    /// Stores informational related properties
+    public var properties: WalletProperties
+
+    /// Returns the encrypted wallet payload - (Base64, AES 256)
+    public var encryptedPayload: String
+
+    /// Returns the user id
+    public var userId: String
+
+    /// Returns the lifetime token for the user id
+    public var lifetimeToken: String
+}
+
+/// Holds credential information regarding the wallet
+public struct WalletCredentials: Equatable {
+    /// Returns the stored wallet identifier
+    public var guid: String
+
+    /// Returns the stored wallet shared key
+    public var sharedKey: String
+
+    /// Returns the stored session token
+    public var sessionToken: String
+
+    /// Returns the stored password
+    public var password: String
+}
+
+/// Holds informational properties regarding the wallet
+public struct WalletProperties: Equatable {
+    /// Returns `true` if the pub keys should be synchronised
+    public var syncPubKeys: Bool
+
+    // Returns the language as specified by the wallet
+    public var language: String
+
+    /// Returns the authenticator type, of the wallet
+    public var authenticatorType: WalletAuthenticatorType
+}
