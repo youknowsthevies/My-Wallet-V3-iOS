d165c5d277ec31b2dd5dae193419108fe619f7f8
dvelasquez-bc <87789177+dvelasquez-bc@users.noreply.github.com>
2021-10-14 14:39:01 -0300

    feat(Components): WACO-16 LoadingStateView (#2873)
    A SwiftUI view which represents a loading state.

---
+++ Modules/UIComponents/UIComponentsKit/Views/LoadingStateView.swift
@@ -0,0 +1,47 @@
+// Copyright © Blockchain Luxembourg S.A. All rights reserved.
+
+import SwiftUI
+
+/// A view which represents a loading state
+public struct LoadingStateView: View {
+
+    let title: String
+
+    private let layout = Layout()
+
+    public init(title: String) {
+        self.title = title
+    }
+
+    public var body: some View {
+        GeometryReader { geometry in
+            VStack {
+                Text(title)
+                    .typography(.title3)
+                    .foregroundColor(.textTitle)
+                ProgressView(value: 0.25)
+                    .progressViewStyle(
+                        IndeterminateProgressStyle(lineWidth: layout.lineWidth)
+                    )
+                    .frame(width: layout.progressViewWidth.in(geometry))
+            }
+            .frame(
+                maxWidth: .infinity,
+                maxHeight: .infinity
+            )
+        }
+    }
+}
+
+extension LoadingStateView {
+    struct Layout {
+        let lineWidth: Length = 12.5.pmin
+        let progressViewWidth: Length = 20.vmin
+    }
+}
+
+struct LoadingStateView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoadingStateView(title: "Loading...")
+    }
+}
