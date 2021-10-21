f32b4b0636527bddfc0282e2dfd54717329a8749
Gianluca <82030034+gtranchedone-bc@users.noreply.github.com>
2021-10-21 13:03:29 +0100

    IOS-5581 Hardcode limits in settings (HOTFIX) (#2933)

--- Modules/FeatureKYC/Sources/FeatureKYCUI/KYCTiersViewController/KYCTiersPageModel.swift
+++ Modules/FeatureKYC/Sources/FeatureKYCUI/KYCTiersViewController/KYCTiersPageModel.swift
@@ -13,16 +13,17 @@ public struct KYCTiersPageModel {
 }
 
 extension KYCTiersPageModel {
+
     var disclaimer: String? {
-        guard let tierTwo = cells.filter({ $0.tier == .tier2 }).first else { return nil }
+        guard let tierTwo = cells.first(where: { $0.tier == .tier2 }) else { return nil }
         guard tierTwo.status != .rejected else { return nil }
         return LocalizationConstants.KYC.completingTierTwoAutoEligible
     }
 
     func trackPresentation(analyticsRecorder: AnalyticsEventRecorderAPI = resolve()) {
         let metadata = cells.map { ($0.tier, $0.status) }
-        guard let tier1 = metadata.filter({ $0.0 == .tier1 }).first else { return }
-        guard let tier2 = metadata.filter({ $0.0 == .tier2 }).first else { return }
+        guard let tier1 = metadata.first(where: { $0.0 == .tier1 }) else { return }
+        guard let tier2 = metadata.first(where: { $0.0 == .tier2 }) else { return }
         let tierOneStatus = tier1.1
         let tierTwoStatus = tier2.1
         switch (tierOneStatus, tierTwoStatus) {
@@ -41,7 +42,7 @@ extension KYCTiersPageModel {
     public static func make(tiers: KYC.UserTiers, maxTradableToday: FiatValue, suppressCTA: Bool) -> KYCTiersPageModel {
         let header = KYCTiersHeaderViewModel.make(
             with: tiers,
-            availableFunds: maxTradableToday.toDisplayString(includeSymbol: true),
+            availableFunds: maxTradableToday.toDisplayString(includeSymbol: true, format: .shortened, locale: .current),
             suppressDismissCTA: suppressCTA
         )
         let models = tiers.tiers
