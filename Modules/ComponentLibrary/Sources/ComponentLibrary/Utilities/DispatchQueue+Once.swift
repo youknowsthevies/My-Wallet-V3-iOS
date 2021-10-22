63c83dabb74ffdb6421a790c66b10add49956b79
Oliver Atkinson <oatkinson@blockchain.com>
2021-10-22 18:04:22 +0100

    [String: Void] -> Set

--- Modules/ComponentLibrary/Sources/ComponentLibrary/Utilities/DispatchQueue+Once.swift
+++ Modules/ComponentLibrary/Sources/ComponentLibrary/Utilities/DispatchQueue+Once.swift
@@ -4,7 +4,7 @@ import Foundation
 
 extension DispatchQueue {
 
-    private static var _onceTracker = [String: Void]()
+    private static var _onceTracker = Set<String>()
 
     /**
       Executes a block of code, associated with a auto generate unique token by file name + fuction name + line of code, only once.  The code is thread safe and will
@@ -27,13 +27,11 @@ extension DispatchQueue {
      - parameter token: A unique reverse DNS style name such as com.vectorform.<name> or a GUID
      - parameter block: Block to execute once
      */
-    public class func once(
-        token: String,
-        block: () -> Void
-    ) {
+    public class func once(token: String, block: () -> Void) {
         objc_sync_enter(self)
         defer { objc_sync_exit(self) }
-        guard _onceTracker[token] != nil else { return }
-        _onceTracker[token] = block()
+        guard _onceTracker.contains(token) else { return }
+        _onceTracker.insert(token)
+        block()
     }
 }
