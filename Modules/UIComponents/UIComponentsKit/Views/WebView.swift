e97222f3807ff66ed1d8434e2cc945923e1a55c0
grayson-bc <grayson@blockchain.com>
2021-11-16 14:25:08 +0000

    IOS-5246: Remove custom user agent

--- Modules/UIComponents/UIComponentsKit/Views/WebView.swift
+++ Modules/UIComponents/UIComponentsKit/Views/WebView.swift
@@ -38,9 +38,6 @@ public final class WebView: NSObject, UIViewRepresentable, WKScriptMessageHandle
                 return configuration
             }()
         )
-        // swiftlint:disable line_length
-        webView.customUserAgent =
-            "Mozilla/5.0 (iPhone; CPU iPhone OS 12_3_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/12.1.1 Mobile/15E148 Safari/604.1"
         self.messageHandlers = messageHandlers
     }
 
