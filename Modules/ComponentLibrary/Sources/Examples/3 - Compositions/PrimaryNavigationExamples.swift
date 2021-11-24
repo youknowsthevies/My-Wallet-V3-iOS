ba4fb7f94f5f7d35d03fd40376bc3c0fdbf79f58
Malcolm Jarvis <87197260+mjarvis-bc@users.noreply.github.com>
2021-11-24 06:23:59 -0800

    chore(Components): IOS-5898 Update primary navigation back button styling (#3187)
    * chore(Components): IOS-5898 Update primary navigation back button styling
    
    * formatting

--- Modules/ComponentLibrary/Sources/Examples/3 - Compositions/PrimaryNavigationExamples.swift
+++ Modules/ComponentLibrary/Sources/Examples/3 - Compositions/PrimaryNavigationExamples.swift
@@ -1,6 +1,6 @@
 // Copyright © Blockchain Luxembourg S.A. All rights reserved.
 
-import ComponentLibrary
+@testable import ComponentLibrary
 import SwiftUI
 
 struct PrimaryNavigationExamples: View {
@@ -38,7 +38,7 @@ struct PrimaryNavigationExamples_Previews: PreviewProvider {
         PrimaryNavigationView {
             PrimaryNavigationExamples()
         }
-        .environment(\.navigationUsesCircledBackButton, false)
+        .environment(\.navigationBackButtonColor, Color(light: .palette.dark400, dark: .palette.white))
         .previewDisplayName("Exchange")
     }
 }
