89835c2f643f35a4989bc53961d675a7c67bee46
Alex McGregor <41585563+thisisalexmcgregor@users.noreply.github.com>
2022-01-10 04:18:36 -0600

    chore(Cherry): IOS-5799 (#3508)
    * chore(Cherry): IOS-5799
    
    * nit
    
    * nits

--- Modules/FeatureInterest/Sources/FeatureInterestDomain/Extensions/CustodialAccountBalance+Conveniences.swift
+++ Modules/FeatureInterest/Sources/FeatureInterestDomain/Extensions/CustodialAccountBalance+Conveniences.swift
@@ -11,25 +11,17 @@ extension CustodialAccountBalance {
     ) {
         guard let balance = response.balance else { return nil }
         let zero: MoneyValue = .zero(currency: currency)
+        let available = response.moneyBalance ?? zero
+        let locked = response.lockedBalance ?? zero
+        // An `Interest` account's withdrawable balance is
+        // the total balance minus the locked funds.
+        // This could also be called `actionableBalance`.
+        let withdrawable = try? available - locked
         self.init(
             currency: currency,
             available: MoneyValue.create(minor: balance, currency: currency) ?? zero,
-            withdrawable: zero,
-            pending: zero
-        )
-    }
-
-    init?(
-        currency: CurrencyType,
-        response: SavingsAccountBalanceDetails
-    ) {
-        guard let balance = response.balance else { return nil }
-        let zero: MoneyValue = .zero(currency: currency)
-        self.init(
-            currency: currency,
-            available: MoneyValue.create(minor: balance, currency: currency) ?? zero,
-            withdrawable: zero,
-            pending: zero
+            withdrawable: withdrawable ?? zero,
+            pending: response.moneyPendingDeposit ?? zero
         )
     }
 }
