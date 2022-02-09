cbd871e9cab98db886b9622efcc43179b98e9814
grayson-bc <grayson@blockchain.com>
2022-02-09 10:24:08 +0000

    IOS-6250 Disable autocorrection on password field

--- Modules/FeatureAuthentication/Sources/FeatureAuthenticationUI/CreateAccount/CreateAccountView.swift
+++ Modules/FeatureAuthentication/Sources/FeatureAuthenticationUI/CreateAccount/CreateAccountView.swift
@@ -127,6 +127,8 @@ private struct CreateAccountForm: View {
             subTextStyle: viewStore.passwordStrength.inputSubTextStyle,
             state: shouldShowError ? .error : .default,
             configuration: {
+                $0.autocorrectionType = .no
+                $0.autocapitalizationType = .none
                 $0.isSecureTextEntry = !viewStore.passwordFieldTextVisible
                 $0.textContentType = .newPassword
             },
