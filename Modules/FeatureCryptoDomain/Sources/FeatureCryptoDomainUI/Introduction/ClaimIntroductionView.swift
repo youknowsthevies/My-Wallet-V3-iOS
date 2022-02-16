943407b00e97744a2a7eb11a8795dcf7fee440af
grayson-bc <grayson@blockchain.com>
2022-02-16 15:37:41 +0000

    IOS-6323 Dummy View

---
+++ Modules/FeatureCryptoDomain/Sources/FeatureCryptoDomainUI/Introduction/ClaimIntroductionView.swift
@@ -0,0 +1,16 @@
+// Copyright © Blockchain Luxembourg S.A. All rights reserved.
+
+import SwiftUI
+
+struct ClaimIntroductionView: View {
+
+    var body: some View {
+        EmptyView()
+    }
+}
+
+struct ClaimIntroductionView_Previews: PreviewProvider {
+    static var previews: some View {
+        ClaimIntroductionView()
+    }
+}
