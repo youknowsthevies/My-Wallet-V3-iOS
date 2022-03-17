81d47f38437e329e95e1a4aef8f0020f35328db9
grayson-bc <83952192+grayson-bc@users.noreply.github.com>
2022-03-17 16:46:21 +0000

    Fix typo

--- Modules/FeatureCryptoDomain/Package.swift
+++ Modules/FeatureCryptoDomain/Package.swift
@@ -82,8 +82,8 @@ let package = Package(
             resources: [
                 // swiftlint:disable line_length
                 .process("Fixtures/GET/explorer-gateway/resolution/ud/search/Searchkey/GET_explorer-gateway_resolution_ud_search_Searchkey.json"),
-                .process("Fixtures/GET/explorer-gateway/resolution/ud/suggestions/Firstname/GET_explorer-gateway_resolution_ud_suggestions_Firstname.json")
-                .process("Fixtures/POST/explorer-gateway/resolution/ud/orders/POST_explorer-gateway_resolution_ud_orders.json"),
+                .process("Fixtures/GET/explorer-gateway/resolution/ud/suggestions/Firstname/GET_explorer-gateway_resolution_ud_suggestions_Firstname.json"),
+                .process("Fixtures/POST/explorer-gateway/resolution/ud/orders/POST_explorer-gateway_resolution_ud_orders.json")
             ]
         ),
         .testTarget(
