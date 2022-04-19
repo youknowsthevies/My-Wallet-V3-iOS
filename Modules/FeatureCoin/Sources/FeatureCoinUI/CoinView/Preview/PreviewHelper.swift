30379e4afeaff630287204ed77642f113da4c9e1
Maciej Burda <mburda@blockchain.com>
2022-04-19 10:42:39 +0100

    Fix after merge

--- Modules/FeatureCoin/Sources/FeatureCoinUI/CoinView/Preview/PreviewHelper.swift
+++ Modules/FeatureCoin/Sources/FeatureCoinUI/CoinView/Preview/PreviewHelper.swift
@@ -27,58 +27,6 @@ extension CryptoCurrency {
     )!
 }
 
-extension Account.Snapshot {
-
-    static var preview = (
-        privateKey: Account.Snapshot.new(
-            id: "PrivateKey",
-            name: "Private Key Wallet",
-            accountType: .privateKey,
-            actions: [.send, .receive, .activity]
-        ),
-        trading: Account.Snapshot.new(
-            id: "Trading",
-            name: "Trading Account",
-            accountType: .trading,
-            actions: [.buy, .sell, .send, .receive, .swap, .activity]
-        ),
-        rewards: Account.Snapshot.new(
-            id: "Rewards",
-            name: "Rewards Account",
-            accountType: .interest,
-            actions: [.rewards.withdraw, .rewards.deposit]
-        ),
-        exchange: Account.Snapshot.new(
-            id: "Exchange",
-            name: "Exchange Account",
-            accountType: .exchange,
-            actions: [.exchange.withdraw, .exchange.deposit]
-        )
-    )
-
-    static func new(
-        id: AnyHashable = "PrivateKey",
-        name: String = "Private Key Wallet",
-        accountType: Account.AccountType = .privateKey,
-        cryptoCurrency: CryptoCurrency = .bitcoin,
-        fiatCurrency: FiatCurrency = .USD,
-        actions: OrderedSet<Account.Action> = [.send, .receive],
-        crypto: MoneyValue = .init(amount: BigInt(123000000), currency: .crypto(.bitcoin)),
-        fiat: MoneyValue = .init(amount: BigInt(4417223), currency: .fiat(.USD))
-    ) -> Account.Snapshot {
-        Account.Snapshot(
-            id: id,
-            name: name,
-            accountType: accountType,
-            cryptoCurrency: cryptoCurrency,
-            fiatCurrency: fiatCurrency,
-            actions: actions,
-            crypto: crypto,
-            fiat: fiat
-        )
-    )!
-}
-
 enum PreviewHelper {
 
     class HistoricalPriceService: HistoricalPriceServiceAPI {
