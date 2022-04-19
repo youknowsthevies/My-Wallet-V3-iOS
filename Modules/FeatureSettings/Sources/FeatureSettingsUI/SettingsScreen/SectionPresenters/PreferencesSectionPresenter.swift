33f1b297d523ae77bdc8a28bd7aa5f2c472ee223
audrea-bc <audrea@blockchain.com>
2022-04-19 10:04:20 +0200

    updated implementation

--- Modules/FeatureSettings/Sources/FeatureSettingsUI/SettingsScreen/SectionPresenters/PreferencesSectionPresenter.swift
+++ Modules/FeatureSettings/Sources/FeatureSettingsUI/SettingsScreen/SectionPresenters/PreferencesSectionPresenter.swift
@@ -2,38 +2,47 @@
 
 import PlatformKit
 import RxSwift
+import ToolKit
+import DIKit
+import Combine
 
 final class PreferencesSectionPresenter: SettingsSectionPresenting {
-
+    
     // MARK: - SettingsSectionPresenting
-
+    
     let sectionType: SettingsSectionType = .preferences
-
-    var state: Observable<SettingsSectionLoadingState> {
-        .just(
-            .loaded(next:
-                .some(
-                    .init(
-                        sectionType: sectionType,
-                        items: [
-                            .init(cellType: .switch(.emailNotifications, emailNotificationsCellPresenter)),
-                            .init(cellType: .badge(.currencyPreference, preferredCurrencyCellPresenter)),
-                            .init(cellType: .common(.notifications))
-                        ]
-                    )
-                )
-            )
-        )
-    }
-
+    
+    var state: Observable<SettingsSectionLoadingState>
+    
     private let emailNotificationsCellPresenter: EmailNotificationsSwitchCellPresenter
     private let preferredCurrencyCellPresenter: PreferredCurrencyCellPresenter
-
+    
     init(
         emailNotificationService: EmailNotificationSettingsServiceAPI,
-        preferredCurrencyBadgeInteractor: PreferredCurrencyBadgeInteractor
+        preferredCurrencyBadgeInteractor: PreferredCurrencyBadgeInteractor,
+        featureFlagService: FeatureFlagsServiceAPI = resolve()
     ) {
         emailNotificationsCellPresenter = .init(service: emailNotificationService)
         preferredCurrencyCellPresenter = .init(interactor: preferredCurrencyBadgeInteractor)
+        
+        var viewModel = SettingsSectionViewModel(
+            sectionType: sectionType,
+            items: [
+                .init(cellType: .switch(.emailNotifications, emailNotificationsCellPresenter)),
+                .init(cellType: .badge(.currencyPreference, preferredCurrencyCellPresenter)),
+            ]
+        )
+        
+        state = featureFlagService.isEnabled(.remote(.applePay))
+            .last()
+            .map { notificationSettingsEnabled -> SettingsSectionLoadingState in
+                
+                let notificationSettingsCell: SettingsCellViewModel = .init(cellType: .common(.notifications))
+                if notificationSettingsEnabled && viewModel.items.contains(notificationSettingsCell) == false {
+                    viewModel.items.append(notificationSettingsCell)
+                }
+                return .loaded(next: .some(viewModel))
+            }
+            .asObservable()
     }
 }
