2d3ebe77dd8557f7e5de9a564195473c3c519269
Maciej Burda <81174411+mburda-bc@users.noreply.github.com>
2022-05-10 14:01:29 +0100

    fix(Email Notification): IOS-6683 Remove spaces and commas from the string representation of the amount (#4023)
    * Remove spaces and commas from the string representation of the amount
    
    * Add simple string representation for MoneyValue

--- Modules/Money/Tests/MoneyKitTests/Balance/CryptoValueDisplayStringTests.swift
+++ Modules/Money/Tests/MoneyKitTests/Balance/CryptoValueDisplayStringTests.swift
@@ -5,7 +5,7 @@ import MoneyKit
 import XCTest
 
 class CryptoValueDisplayStringTests: XCTestCase {
-    func testBTC() {
+    func test_displayString() {
         let minor = CryptoValue.create(minor: BigInt("1"), currency: .bitcoin)
         let cases: [(locale: Locale, string: String)] = [
             (.US, "0.00000001 BTC"),
@@ -23,5 +23,18 @@ class CryptoValueDisplayStringTests: XCTestCase {
                 "\(this.locale), \(this.string): got \(result) instead"
             )
         }
+    }
+
+    func test_simpleString() {
+        let btc = MoneyValue(cryptoValue: CryptoValue.create(minor: BigInt("91234791789241"), currency: .bitcoin))
+        let eth = MoneyValue(cryptoValue: CryptoValue.create(minor: BigInt("791789241"), currency: .ethereum))
+        let usd = MoneyValue(fiatValue: FiatValue.create(minor: BigInt("89241"), currency: .USD))
+
+        XCTAssertEqual(btc.toSimpleString(includeSymbol: false), "912347.91789241")
+        XCTAssertEqual(btc.toSimpleString(includeSymbol: true), "912347.91789241 BTC")
+        XCTAssertEqual(eth.toSimpleString(includeSymbol: false), "0.000000000791789241")
+        XCTAssertEqual(eth.toSimpleString(includeSymbol: true), "0.000000000791789241 ETH")
+        XCTAssertEqual(usd.toSimpleString(includeSymbol: false), "892.41")
+        XCTAssertEqual(usd.toSimpleString(includeSymbol: true), "892.41 USD")
     }
 }
